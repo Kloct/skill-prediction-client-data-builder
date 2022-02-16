1f0d20033c14cf91d35ba92adce48fa0d4585710
Kloct <Upfrontsuperior@gmail.com>
2022-02-16 16:51:50 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,146 @@
+const fs = require('fs')
+const Abnormality = fs.readdirSync("./DC/Abnormality")
+
+/**
+ * Abnormality
+ */
+let abnormalities = {}
+for (let file of Abnormality) {
+    const { Abnormal } = require(`./DC/Abnormality/${file}`)
+    if (Abnormal != undefined) {
+        for (let abnormal of Abnormal) {
+            if (abnormal.AbnormalityEffect == undefined) {
+                continue;
+            }
+            let bySkillCategory = []
+            try {
+                bySkillCategory = abnormal.bySkillCategory.split(",").map(e => parseInt(e));
+            } catch { }
+            let effects = []
+            for (let effect of abnormal.AbnormalityEffect) {
+                effects.push({ type: effect.type, method: effect.method, value: (new Number(effect.value)) })
+            }
+            abnormalities[abnormal.id] = { bySkillCategory, effects }
+
+        }
+    }
+
+}
+fs.writeFileSync("./abnormality.json", JSON.stringify(abnormalities))
+
+const EpPerk = require("./DC/EpPerk.json")
+const Passivity = mergePassivity("./DC/Passivity")
+
+/**
+ * ep and ep_pog
+ */
+
+function mergePassivity(path) {
+    let files = fs.readdirSync(path)
+    let mergedPassive = []
+    for (let file of files) {
+        mergedPassive = [...require(`${path}/${file}`).Passive, ...mergedPassive]
+    }
+    return mergedPassive
+}
+
+let ep_pog = {}
+let ep = {}
+for (let perk of EpPerk.Perk) {
+    ep_pog[`${perk.id},${perk.level}`] = []
+    ep[`${perk.id},${perk.level}`] = []
+    for (let data of perk.Condition[1].Data) {
+        for (let passive of Passivity) {
+            if (passive.id == data.id) {
+                ep_pog[`${perk.id},${perk.level}`].push({
+                    type: passive.type,
+                    method: passive.method,
+                    value: new Number(data.value),
+                    ...(passive.condition != 0) && { condition: passive.condition },
+                    ...(passive.conditionValue != 0) && { conditionValue: new Number(passive.conditionValue) },
+                    ...(checkConditionCategory(passive.conditionCategory)) && { conditionCategory: passive.conditionCategory.split(",").map(e => parseInt(e)) }
+                })
+                if (passive.conditionCategory.split(',').length == 1 && parseInt(passive.conditionCategory) != 0) {
+                    ep[`${perk.id},${perk.level}`].push({
+                        conditionCategory: parseInt(passive.conditionCategory),
+                        type: passive.type,
+                        method: passive.method,
+                        value: new Number(data.value)
+                    })
+                }
+            }
+        }
+    }
+    if (ep[`${perk.id},${perk.level}`].length == 0) delete ep[`${perk.id},${perk.level}`]
+}
+function checkConditionCategory(conditionCategory) {
+    try {
+        if (parseInt(conditionCategory) == 0) return false
+    } catch { return false }
+    return true
+}
+fs.writeFileSync("./ep_pog.json", JSON.stringify(ep_pog, null, 1))
+fs.writeFileSync("./ep.json", JSON.stringify(ep, null, 0))
+
+/**
+ * passivity
+ */
+let passivity = {}
+for (let passive of Passivity) {
+    if (parseInt(passive.conditionCategory) != 0 && parseInt(passive.conditionCategory) != NaN) {
+        passivity[passive.id] = {
+            conditionCategory: parseInt(passive.conditionCategory),
+            type: passive.type,
+            method: passive.method,
+            value: new Number(passive.value)
+
+        }
+    }
+}
+fs.writeFileSync("./passivity.json", JSON.stringify(passivity, null, 0))
+
+/**
+ * polishing
+ */
+const { SkillPolishingEffect } = require("./DC/SkillPolishingEffectList.json")
+
+let polishing = {}
+for (let effect of SkillPolishingEffect) {
+    polishing[effect.id] = []
+    for (let passive of effect.Cost[1].Passive) {
+        polishing[effect.id].push(passive.id)
+    }
+}
+fs.writeFileSync("./polishing.json", JSON.stringify(polishing, null, 0))
+
+/**
+ * noctan
+ */
+const ClassIDs = [
+    "warrior",
+    "lancer",
+    "slayer",
+    "berserker",
+    "sorcerer",
+    "archer",
+    "priest",
+    "elementalist",
+    "soulless",
+    "engineer",
+    "fighter",
+    "assassin",
+    "glaiver"
+]
+const { NoctanPerformanceSet } = require("./DC/NocTanData.json")
+const effectThreshold = 1.0
+let noctan = {}
+for (let performanceSet of NoctanPerformanceSet) {
+    if (performanceSet.NocTanSkillData)
+        for (let skillData of performanceSet.NocTanSkillData) {
+            if (new Number(skillData.effectRate) > effectThreshold && skillData.effectType == 3) {
+                if (!noctan[ClassIDs.indexOf(skillData.class)]) noctan[ClassIDs.indexOf(skillData.class)] = {}
+                noctan[ClassIDs.indexOf(skillData.class)][skillData.skillId] = new Number(skillData.effectRate)
+            }
+        }
+}
+fs.writeFileSync("./noctan.json", JSON.stringify(noctan, null, 0))
